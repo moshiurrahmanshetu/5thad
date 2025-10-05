10559b6163137e9ab603dee34e942d7e3f719762
moshiurrahmanshetu <shetumoshiurrahman@gmail.com>
2025-10-05 21:23:50 +0600

    blog page complete

--- blog.php
+++ blog.php
@@ -7,14 +7,17 @@
     
   <section class="blog-section py-5">
     <div class="container">
-      <h2 class="mb-4 text-cm-steel-blue text-center">Our Blogs</h2>
+      <h2 class="mb-4 text-center">Our Blogs</h2>
       <div class="row g-4 justify-content-center">
         <div class="col-12 col-md-6 col-lg-4">
           <a href="#">
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-1.jpg" class="card-img-top" alt="Analytics">
                 <div class="card-body d-flex flex-column">
-                <span class="text-uppercase text-cm-steel-blue mb-1 small">Insights</span>
+                  <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
                 <h5 class="card-title text-cm-d-blue">Analytics for Smarter Business Decisions and Growth</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
@@ -27,7 +30,11 @@
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-2.jpg" class="card-img-top" alt="Analytics">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title text-cm-d-blue">Analytics</h5>
+                <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
+                <h5 class="card-title text-cm-d-blue">Analytics for Smarter Business</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
               </div>
@@ -39,7 +46,11 @@
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-3.jpg" class="card-img-top" alt="Analytics">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title text-cm-d-blue">Analytics</h5>
+                <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
+                <h5 class="card-title text-cm-d-blue">Analytics for Smarter Business Decisions</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
               </div>
@@ -51,7 +62,11 @@
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-4.jpg" class="card-img-top" alt="Analytics">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title text-cm-d-blue">Analytics</h5>
+                <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
+                <h5 class="card-title text-cm-d-blue">Analytics for Smarter</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
               </div>
@@ -63,7 +78,11 @@
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-5.jpg" class="card-img-top" alt="Analytics">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title text-cm-d-blue">Analytics</h5>
+                <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
+                <h5 class="card-title text-cm-d-blue">Analytics for Smarter Business Decisions and Growth</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
               </div>
@@ -75,7 +94,11 @@
             <div class="card h-100 blog-feature-card">
               <img src="assets/blog-6.jpg" class="card-img-top" alt="Analytics">
               <div class="card-body d-flex flex-column">
-                <h5 class="card-title text-cm-d-blue">Analytics</h5>
+                <div class="category-date d-flex justify-content-between mb-3">
+                    <span class="text-uppercase mb-1 small">Insights</span>
+                    <span class="text-uppercase mb-1 small">05-09-2025</span>
+                  </div>
+                <h5 class="card-title text-cm-d-blue">Analytics for Smarter Business Decisions and Growth for Smarter</h5>
                 <p class="card-text text-secondary">Understand your data with clear visuals. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis dolore totam, aperiam tempora a eligendi excepturi ut explicabo saepe pariatur sunt voluptatum. Commodi, optio nobis.</p>
                 <button class="btn mt-auto mt-3">Learn More</button>
               </div>
